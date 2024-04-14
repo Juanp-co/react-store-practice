e5108668f506ac9869efa9ad44c2b968f8a787a5
Juan Piñeros <juansepinerosh@gmail.com>
2024-04-14 09:58:55 -0700

    feature/implement-styled-components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,40 @@ import DirectoryItem from '../directory-item/directory-item.component';
 
 import './directory.styles.scss';
 
-const Directory = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+    route: '/shop/hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    route: '/shop/jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    route: '/shop/sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    route: '/shop/womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    route: '/shop/mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+  },
+];
+
+const Directory = () => {
   return (
     <div className='directory-container'>
       {categories.map((category) => (
